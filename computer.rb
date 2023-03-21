f2dbf68a745c8223c8afed5476e3ab63cc842452
Alfredo Ormeno <ormenoandres13@gmail.com>
2023-03-20 22:09:04 -0400

    Add random code generator for the computer class

--- computer.rb
+++ computer.rb
@@ -3,5 +3,12 @@ class Computer
   def initialize(game)
     @game = game
     @guess = []
+    @secret_code = []
+  end
+
+  def generate_code
+    4.times do 
+      @secret_code.push(@game.KEYS[rand(6)])
+    end
   end
 end
